28d2f03c2b9aa05b4084d8178de27f653d201d61
Liam-HeatEng <140842289+Liam-HeatEng@users.noreply.github.com>
2024-02-18 06:01:28 +0000

    add forfeit and medals

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,10 +13,23 @@ function App() {
   const [timeLeft, setTimeLeft] = React.useState(60)
   const [usedWords, setUsedWords] = React.useState([])
   const [round, setRound] = React.useState(0)
+  const [record, setRecord] = React.useState(0);
+  const [recordBeat, setRecordBeat] = React.useState(false);
+
+  React.useEffect(() => {
+    if (
+      !localStorage.getItem("record") ||
+      Number(playerScore) > Number(localStorage.getItem("record"))
+    ) {
+      localStorage.setItem("record", score.toString());
+      setRecordBeat(true);
+    }
+    setRecord(localStorage.getItem("record"));
+  }, [playerScore]);
 
   return (
     <div className="bg-gray-900 flex flex-col justify-center items-center text-center w-screen h-screen overflow-hidden">
-    <GameContext.Provider value={{gameState, setGameState, playerScore, setPlayerScore, defeatReason, setDefeatReason, timeLeft, setTimeLeft, usedWords, setUsedWords, round, setRound}}>
+    <GameContext.Provider value={{gameState, setGameState, playerScore, setPlayerScore, defeatReason, setDefeatReason, timeLeft, setTimeLeft, usedWords, setUsedWords, round, setRound, record, setRecord, recordBeat, setRecordBeat}}>
     {gameState === 'splash' && <Splash />}
     {gameState === 'game' && <Game />}
     {gameState === 'results' && <Results />}

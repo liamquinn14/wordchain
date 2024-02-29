b299719e160cd2db4f666f7320db9ebf9ebc983e
Liam-HeatEng <140842289+Liam-HeatEng@users.noreply.github.com>
2024-02-29 14:29:33 +0000

    console log api response

--- server/server.js
+++ server/server.js
@@ -39,7 +39,8 @@ app.post('/api/testPlayerAnswer', async (req, res) => {
             ],
             temperature: 0,
         });
-        const confirmedWord = response.data.choices[0].text.trim().split("*")
+        const confirmedWord = response
+        console.log(response)
         //.split("*")[1]?.toLowerCase().replace(/'/gm, '');
         res.json({confirmedWord: confirmedWord});
         console.log("test complete");
